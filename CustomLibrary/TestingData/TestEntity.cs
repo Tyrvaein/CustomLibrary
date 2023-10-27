94ef54601926e0bf914bde163d28949dfaa7c92a
Steven Grabbert <spam@steven-grabbert.de>
2023-10-27 19:56:25 +0200

    Rename

---
+++ CustomLibrary/TestingData/TestEntity.cs
@@ -0,0 +1,21 @@
+﻿using CustomLibrary.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCustomLibrary.TestData
+{
+	public class TestEntity
+	{
+		[Mapping("Id")]
+		public int Id { get; set; } = 1;
+		public string Name { get; set; } = "Entity";
+		public string Description { get; set; } = "This is an Entiyt";
+		public string Type { get; set; } = "Entity";
+
+		[Mapping("Date")]
+		public DateTime DateTime { get; set; } = DateTime.Now;
+	}
+}
---
+++ CustomLibrary/TestingData/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using CustomLibrary.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCustomLibrary.TestData
+{
+	public class TestModel
+	{
+		[Mapping("Id")]
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		[Mapping("Type", "HH:mm:ss")]
+		public string? Type { get; set; }
+
+		[Mapping("Date","HH:mm:ss")]
+		public DateTime DateTime { get; set; }
+	}
+}
